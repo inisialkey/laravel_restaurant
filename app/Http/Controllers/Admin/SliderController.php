9b15951d478332f3729334757e5ec6def93adb2c
okidorvel <okidorvel404@gmail.com>
2019-09-10 16:17:14 +0700

    add show slider in frontend page

--- app/Http/Controllers/Admin/SliderController.php
+++ app/Http/Controllers/Admin/SliderController.php
@@ -132,7 +132,9 @@ class SliderController extends Controller
     public function destroy($id)
     {
         $slider = Slider::find($id);
-        unlink('uploads/slider/'. $slider->image);
+        if (file_exists('uploads/slider/'. $slider->image)) {
+            unlink('uploads/slider/'. $slider->image);
+        }
         $slider->delete();
         return redirect()->back()->with('successMsg', 'Slider Successfuly Deleted');
     }
