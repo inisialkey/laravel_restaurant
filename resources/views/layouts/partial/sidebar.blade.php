9b9b3c828063197d8b0fef3a21a6679f5a411a14
okidorvel <okidorvel404@gmail.com>
2019-09-12 00:38:56 +0700

    fix theme bug

--- resources/views/layouts/partial/sidebar.blade.php
+++ resources/views/layouts/partial/sidebar.blade.php
@@ -5,8 +5,8 @@
                         Tip 2: you can also add an image using data-image tag
                     -->
     <div class="logo">
-        <a href="http://www.creative-tim.com" class="simple-text logo-normal">
-            Creative Tim
+        <a href="{{ route('admin.dashboard') }}" class="simple-text logo-normal">
+            Management Resto
         </a>
     </div>
     <div class="sidebar-wrapper">
